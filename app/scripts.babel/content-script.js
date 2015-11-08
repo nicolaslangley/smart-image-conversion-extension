63138cc1b022697969998bed17c481f5c46f07ce
Nicolas Langley <nicolasqlangley@gmail.com>
2015-11-07 19:43:30 -0800

    Renamed functions to camel case

--- app/scripts.babel/content-script.js
+++ app/scripts.babel/content-script.js
@@ -1,7 +1,7 @@
 
-var all_images = Array.prototype.slice.call(document.getElementsByTagName("img"));
-console.log(all_images.length);
-var type_output = "jpeg"; // Possible values are gif, png, tiff, jpeg, bmp
+var allImages = Array.prototype.slice.call(document.getElementsByTagName("img"));
+console.log(allImages.length);
+var typeOutput = "jpeg"; // Possible values are gif, png, tiff, jpeg, bmp
 
 var convert = new Interface(chrome.runtime.getURL('image_magick/scripts/convert-worker.js'));
 convert.on_stdout = function(txt) { console.log(txt); };
@@ -13,21 +13,22 @@ convert.addUrl('../config/english.xml', '/usr/local/etc/ImageMagick/');
 convert.addUrl('../config/locale.xml',  '/usr/local/etc/ImageMagick/');
 convert.addUrl('../config/delegates.xml',  '/usr/local/etc/ImageMagick/');
 
-all_images.forEach(function(img_input, index) {
-    var url = img_input.src;
+// TODO: this function skips images in replacement - may only do last image
+allImages.forEach(function(imgInput, index) {
+    var url = imgInput.src;
     convert.addUrl(url, '/');
-    var split_filename = url.substring(url.lastIndexOf('/')+1).split('.');
-    var filename = split_filename[0]; // TODO: verify that there are two elements  
-    var type_input = split_filename[1];
+    var splitFilename = url.substring(url.lastIndexOf('/')+1).split('.');
+    var filename = splitFilename[0]; // TODO: verify that there are two elements  
+    var typeInput = splitFilename[1];
     convert.allDone().then(function() {
-        convert.run('/'+filename+"."+type_input, '/'+filename+"."+type_output).then(function() {
-            convert.getFile(filename+"."+type_output).then(function(img_data) {
-                var img_output = document.createElement("img");
-                img_output.id = "received";
-                img_output.src = "data:image/"+type_output+";base64,"+window.btoa(img_data);
-                var parent_node = img_input.parentNode;
-                parent_node.removeChild(img_input);
-                parent_node.appendChild(img_output);
+        convert.run('/'+filename+"."+typeInput, '/'+filename+"."+typeOutput).then(function() {
+            convert.getFile(filename+"."+typeOutput).then(function(imgData) {
+                var imgOutput = document.createElement("img");
+                imgOutput.id = "received";
+                imgOutput.src = "data:image/"+typeOutput+";base64,"+window.btoa(imgData);
+                var parentNode = imgInput.parentNode;
+                parentNode.removeChild(imgInput);
+                parentNode.appendChild(imgOutput);
             });
         });
     });
