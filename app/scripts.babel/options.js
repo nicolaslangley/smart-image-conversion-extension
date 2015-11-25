42dfa4e9dd1c3fe24409851554c2bb3893646f7c
Nicolas Langley <nicolasqlangley@gmail.com>
2015-11-25 14:00:55 -0800

    Added new selection options Start of #9

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -2,11 +2,19 @@
 
 // Saves options to chrome.storage
 function save_options() {
-  var selected = $('#formatOptions input:radio:checked').val();
-  console.log(selected);
-  var format = selected;
+  var format = $('#formatOptions input:radio:checked').val();
+  var compression = $('#compressionOptions input:radio:checked').val();
+  var os = $('#operatingSystemOptions input:radio:checked').val();
+  var colorSetting = $('#colorSpaceOptions input:radio:checked').val();
+  console.log("Saving to format " + format);
+  console.log("Saving compression to " + compression);
+  console.log("Saving to operating system " + os);
+  console.log("Saving to color range " + colorSetting);
   chrome.storage.sync.set({
     outputFormat: format,
+    compressionType: compression,
+    operatingSystem: os,
+    colorRange: colorSetting
   }, function () {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -23,9 +31,18 @@ function restore_options() {
   console.log("Restoring options...");
   chrome.storage.sync.get({
     outputFormat: 'jpeg',
+    compressionType: 'lossless',
+    operatingSystem: 'linux',
+    colorRange: 'nocolorhigh'
   }, function (items) {
     console.log("Restoring to format " + items.outputFormat);
+    console.log("Restoring compression to " + items.compressionType);
+    console.log("Restoring to operating system " + items.operatingSystem);
+    console.log("Restoring to color range " + items.colorRange);
     $('#' + items.outputFormat).closest('.btn').button('toggle');
+    $('#' + items.compressionType).closest('.btn').button('toggle');
+    $('#' + items.operatingSystem).closest('.btn').button('toggle');
+    $('#' + items.colorRange).closest('.btn').button('toggle');
   });
 }
 
